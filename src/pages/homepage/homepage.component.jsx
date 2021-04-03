c936cecf87383a36fa69bc5c8139a43b40aef272
Talal Anwar <talal.anwar@icommunix.com>
2021-04-03 17:49:35 +0500

    broken our homepage into smaller components, finished styling our menu-item.

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import Directory from "../../components/directory/directory.component";
+import "./homepage.styles.scss";
+
+export const HomePage = () => {
+	return (
+		<div className="homepage">
+			<Directory />
+		</div>
+	);
+};
+
+export default HomePage;
