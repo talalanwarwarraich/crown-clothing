7fe5a6b8532f406881a5a7a1cc1ef2ca8a2f8b86
Talal Anwar <talal.anwar@icommunix.com>
2021-04-12 19:29:15 +0500

    setting user state which we are getting from firebase and storing in our local state to identify if user is signed in or not.. and creating new user in our database if the user is logging in for the first time.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component.jsx";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -21,16 +21,25 @@ class App extends Component {
 		this.listenFirebaseAuthEvent();
 	}
 
-	listenFirebaseAuthEvent = () => {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
-			console.log("currentUser", user);
-		});
-	};
-
 	componentWillUnmount() {
 		this.unsubscribeFromAuth();
 	}
+
+	listenFirebaseAuthEvent = () => {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) this.setAuthUserState(userAuth);
+			else this.setState({ currentUser: userAuth });
+		});
+	};
+
+	setAuthUserState = async (userAuth) => {
+		const userRef = await createUserProfileDocument(userAuth);
+		userRef.onSnapshot((snapShot) => {
+			this.setState({
+				currentUser: { id: snapShot.id, ...snapShot.data() },
+			});
+		});
+	};
 
 	render() {
 		return (
