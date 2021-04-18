0e0439a8eb08ac0549f339e15d524f4cd7200684
Talal Anwar <talal.anwar@icommunix.com>
2021-04-19 00:49:11 +0500

    removed extra commented code form header.component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
@@ -6,44 +5,6 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropDown from "../cart-dropdown/cart-dropdown.component";
-import { toggleCartDropdown } from "../../redux/cart/cart.actions";
-
-// class Header extends Component {
-// 	handleCartDropDownToggle = () => {
-// 		const { toggleCartDropdown } = this.props;
-// 		toggleCartDropdown();
-// 	};
-
-// 	render() {
-// 		const { currentUser, hidden } = this.props;
-// 		return (
-// 			<div className="header">
-// 				<Link className="logo-container" to="/">
-// 					<Logo className="logo" />
-// 				</Link>
-// 				<div className="options">
-// 					<Link className="option" to="/shop">
-// 						SHOP
-// 					</Link>
-// 					<Link className="option" to="/shop">
-// 						CONTACT
-// 					</Link>
-// 					{currentUser ? (
-// 						<div className="option" onClick={() => auth.signOut()}>
-// 							SIGN OUT
-// 						</div>
-// 					) : (
-// 						<Link className="option" to="/signin">
-// 							SIGN IN
-// 						</Link>
-// 					)}
-// 					<CartIcon onClick={this.handleCartDropDownToggle} />
-// 				</div>
-// 				{!hidden ? <CartDropDown /> : null}
-// 			</div>
-// 		);
-// 	}
-// }
 
 const Header = ({ currentUser, hidden }) => {
 	return (
