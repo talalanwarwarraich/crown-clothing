6db4f441b112f719065b6cb44c8faf8a5ea3a95a
Talal Anwar <talal.anwar@icommunix.com>
2021-06-09 16:19:58 +0500

    added directory and shop reducers/selectors, and created CollectionOverview component for displaying shop data.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,25 +1,21 @@
-import { Component } from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+
 import MenuItem from "../menu-item/menu-item.component";
 import "./direcotry.styles.scss";
-import SECTIONS_DATA from "../../data/sections.data";
 
-class Directory extends Component {
-	constructor() {
-		super();
-		this.state = {
-			sections: SECTIONS_DATA,
-		};
-	}
+const Directory = ({ sections }) => (
+	<div className="directory-menu">
+		{sections.map(({ id, ...otherSectionProps }) => (
+			<MenuItem key={id} {...otherSectionProps} />
+		))}
+	</div>
+);
 
-	render() {
-		return (
-			<div className="directory-menu">
-				{this.state.sections.map(({ id, ...otherSectionProps }) => (
-					<MenuItem key={id} {...otherSectionProps} />
-				))}
-			</div>
-		);
-	}
-}
+const mapStateToProps = createStructuredSelector({
+	sections: selectDirectorySections,
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
